388103607f53bbea2224f21ef6f1c8cfb28b393a
ThaliaRowan <thalia.rowan@gmail.com>
2021-06-03 16:30:47 -0300

    finished home page

--- projectmanagement/src/pages/Home.js
+++ projectmanagement/src/pages/Home.js
@@ -1,14 +1,34 @@
 import React from "react";
-import {Row, Col} from "react-bootstrap";
-
+import {Row, Col, Image, Button} from "react-bootstrap";
+import Desk from "../assets/desk.jpg";
+import  "../Home.css";
 
 function Home(){
     return(
         <div>
             <Row>
                 <Col>
+                    <div id="imgDiv">
+                        <Image src={Desk} style={{width: "50rem"}} />
+                    </div>
                 </Col>
                 <Col>
+                <Row id="titleDiv">
+                    <h1 id="title">
+                        TASKAGE
+                    </h1>
+                </Row>
+                <div id="textDiv">
+                    <p id="intro">Make self-management easier</p>
+                </div>
+                <div>
+                    <div id="signupDiv">      
+                    <Button variant="outline-secondary" id="signup">Signup</Button>
+                    </div>
+                    <div id="loginDiv">
+                    <Button variant="outline-secondary" id="login">Login</Button>
+                    </div>
+                </div>
                 </Col>
             </Row>
         </div>
