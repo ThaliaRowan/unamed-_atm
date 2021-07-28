66ef0e46fe7f20346bd874d295f8f0780035e7eb
ThaliaRowan <thalia.rowan@gmail.com>
2021-07-28 15:45:06 -0300

    opacity changes when td is dragged

--- projectmanagement/src/pages/Board.js
+++ projectmanagement/src/pages/Board.js
@@ -1,8 +1,39 @@
-import React from "react";
+import React, {useRef, useEffect, useState} from "react";
 import {Row, Button, Col,Table, Navbar, Container} from "react-bootstrap";
 import "../Board.css";
 
 function Board() {
+
+    const [isTrue, setTrue] = useState(false);
+
+    let draggables = useRef();
+    const container = useRef();
+
+
+        function addDrag(){
+           setTrue(true);
+        }
+
+        function removeDrag(){
+            setTrue(false)
+        }
+
+
+        useEffect(() => {
+
+            window.addEventListener('dragstart', addDrag);
+            window.addEventListener('dragend', removeDrag);
+            return () => {
+                
+            window.removeEventListener('dragstart', addDrag);
+            window.addEventListener('dragend', removeDrag);
+            }
+
+
+        }, []);
+
+
+
     return(
         <div>
             <Navbar expand="lg" variant="light" bg="light">
@@ -28,16 +59,20 @@ function Board() {
                             </thead>
                             <tbody className="tableB">
                             <tr className="tableR">
-                                    <td>
-                                        <div className="tableD">
+                                    <td  className="draggable" ref={container} >
+                                        <div className={ isTrue ? "dragging" : "tableD"} draggable="true" ref={draggables}>
                                             write css
                                         </div>
                                     </td>
-                                    <td>
+                                    <td >
+                                        <div draggable="true" className="draggable">
 
+                                        </div>
                                     </td>
                                     <td>
-                                        
+                                        <div  draggable="true" className="draggable">
+
+                                        </div>
                                     </td>
                             </tr>
                         </tbody>
